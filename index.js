39353bbd581ffc77187c29bcd4cae0f880bff6b4
Sophie Gu <sophieqgu@gmail.com>
2021-01-03 05:10:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ function greet(timeString) {
 
 /* Write your implementation of displayMessage() */
 function displayMessage(message) {
-  document.getElementById('greeting').innerHTML = message;
+  document.getElementById('greeting').innerText = message;
 }
