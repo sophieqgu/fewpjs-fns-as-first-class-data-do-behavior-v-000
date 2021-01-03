538bd6fe824f82577a90b2e518161a91a36af45c
Sophie Gu <sophieqgu@gmail.com>
2021-01-03 05:18:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 function greet(timeString) {
-  timeString = parseInt(timeString)
-  if (timeString >= 0 && timeString < 12) {
+  let time = parseInt(timeString)
+  if (time < 12) {
     return "Good Morning";
-  } else if (timeString < 17) {
+  } else if (time < 17) {
     return "Good Afternoon";
   } else {
     return "Good Evening";
